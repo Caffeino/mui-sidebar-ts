eecf5269c246d9db6aa9e68139e888e158085a2c
Andres Tellez <andres.tezper@gmail.com>
2024-11-16 05:37:18 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+function App() {
+	return (
+		<Typography variant='h6' gutterBottom>
+			MUI SideBar With React + TS
+		</Typography>
+	);
+}
+
+export default App;
